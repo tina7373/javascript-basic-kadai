a9410f3d8504101eadb7d5517bcf2f7d07b75898
tina7373 <chinatsu.sora.cute@gmail.com>
2024-12-20 00:02:33 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,12 @@
+function todaysDate(){ //todaysDate関数
+
+const today = new Date(); //Dateクラス
+
+const year = today.getFullYear(); //年
+const month = today.getMonth(); //月
+const date = today.getDate(); //日
+
+console.log(year + '年' + month + '月' + date + '日'); //コンソールに出力
+}
+
+todaysDate(); //呼び出し
