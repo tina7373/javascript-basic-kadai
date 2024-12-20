3f4241f8545f9da54ddde8e200e47aecc0d0dcee
tina7373 <chinatsu.sora.cute@gmail.com>
2024-12-20 09:15:15 +0900

    Update mdn.js
    fix the definition of MONTH

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -3,7 +3,7 @@ function todaysDate(){ //todaysDate関数
 const today = new Date(); //Dateクラス
 
 const year = today.getFullYear(); //年
-const month = today.getMonth(); //月
+const month = today.getMonth() + 1; //月
 const date = today.getDate(); //日
 
 console.log(year + '年' + month + '月' + date + '日'); //コンソールに出力
